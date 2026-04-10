fdaff3d224688d167e20ef7ae6212248e540920b
sabnamparvinbristy <parvin.sabnam.002@gmail.com>
2026-04-10 23:58:38 +0600

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function WeatherApp() {
+  return (
+    <div className="app">
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
+    </div>
+  );
+}
